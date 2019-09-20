f87e3391e159137e49b9c33d9223e615cbb68597
CaptainSword123 <37672722+CaptainSword123@users.noreply.github.com>
2019-09-20 22:14:09 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,5 +1,5 @@
 import urllib.request, json
-
+import time
 with urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q=Paderborn&appid=d3854ae2f0e16209429220f480a775f0") as url:
     data = json.loads(url.read().decode())
     #print(data)
@@ -9,5 +9,8 @@ celcius = celcius * 10
 celcius = round(celcius)
 celcius = celcius / 10
 
-print(celcius)
+time = time.strftime("%H:%M %d.%m.%Y")
+date = time.__str__()[6:]
+time = time.__str__()[:-11]
 
+print("In Paderborn ist es %s Uhr am %s. Die Temperatur beträgt %s °C" % (time, date, celcius))
