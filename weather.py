9e00b5556396ce491d6ab5de9e987c18552d426f
CaptainSword123 <37672722+CaptainSword123@users.noreply.github.com>
2019-09-20 21:59:41 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 import urllib.request, json
 
-with urllib.request.urlopen("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22") as url:
+with urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q=Paderborn&appid=d3854ae2f0e16209429220f480a775f0") as url:
     data = json.loads(url.read().decode())
     #print(data)
 celvin = (data["main"]["temp"])
