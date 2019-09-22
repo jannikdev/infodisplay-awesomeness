1e29d472edb19997085d78c6effd69290991d276
CaptainSword123 <37672722+CaptainSword123@users.noreply.github.com>
2019-09-22 10:21:00 +0200

    Update app.py

--- server/app.py
+++ server/app.py
@@ -6,7 +6,18 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/')
 
 def hello():
+    if int(GetEmails()[4]) > 0:
+        bottom = '<div class="bottom">Sie haben 0 neue Nachrichten, deswegen die aktuellsten News: %s</div>' % str(GetNews())
+        index = "index"
+    elif int(GetEmails()[4]) == 1:
+        bottom = '<div>%s neue Emails:</<div>\n<div>%s: %s</div>' % (str(GetEmails()[4]), str(GetEmails()[0]), str(GetEmails()[1]))
+        index = "indexmail"
+    else:
+        bottom = '<div>%s neue Emails:</<div>\n<div>%s: %s</div>\n<div>%s: %s</<div>' % (str(GetEmails()[4]), str(GetEmails()[0]), str(GetEmails()[1]), str(GetEmails()[2]), str(GetEmails()[3]))
+        index = "indexmail"
     templateData = {
+        'index' : index,
+        'bottom' : bottom,
         'title' : 'Infodisplay',
         'time': str(GetTime()),
         'celsius': str(GetTemperature()) + "°C",
